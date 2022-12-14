522c33a7220cfda01d36b49af29c3444cdada128
anuraglalwani <anuraglalwani30@gmail.com>
2022-12-14 16:00:46 +0530

    i commmit

--- index.js
+++ index.js
@@ -17,16 +17,16 @@ const port= process.env.PORT||8001;
 app.use(express.json());
 app.use(Cors());
 
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "./client/build")));
-app.get("*", function (_, res) {
-  res.sendFile(
-    path.join(__dirname, "./client/build/index.html"),
-    function (err) {
-      res.status(500).send(err);
-    }
-  );
-});
+// const __dirname = path.resolve();
+// app.use(express.static(path.join(__dirname, "./client/build")));
+// app.get("*", function (_, res) {
+//   res.sendFile(
+//     path.join(__dirname, "./client/build/index.html"),
+//     function (err) {
+//       res.status(500).send(err);
+//     }
+//   );
+// });
 
 //db config
 var mongoDB = process.env.URI;
